55d597e3dc168a1d5d52392e2f0f6a1b3c10a843
suhaspirankar <Suhas2190@gmail.com>
2016-02-07 22:05:02 -0500

    commit by suhas

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,14 +1,37 @@
-﻿namespace FizzBuzz
+﻿
+using System;
+
+namespace FizzBuzz
 {
-	public class FizzBuzz
-	{
-		public string RunFizzBuzz(int number)
-		{
-			string result = number.ToString();
+    public class FizzBuzz
+    {
+        public string RunFizzBuzz(int number)
+        {
+            string result = number.ToString();
 
-			// your solution/implementation should go in here
 
-			return result;
-		}
-	}
+
+            if (number == 0)
+            {
+                return result; //  checking if number is 0
+
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                result = "FizzBuzz";
+            }
+
+            else if (number % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                result = "Buzz";
+
+            }
+            return result;
+        }
+    }
 }
+
