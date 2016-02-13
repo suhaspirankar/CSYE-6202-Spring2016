a3bf7d3332c7a77ca0c27d9744c977c413418b96
suhaspirankar <Suhas2190@gmail.com>
2016-02-12 22:01:57 -0500

    Final_Commit_suhas

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -2,38 +2,106 @@
 
 namespace TrianglePatterns
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			DisplayPatternA();
-			DisplayPatternB();
-			DisplayPatternC();
-			DisplayPatternD();
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-			Console.ReadLine();
-		}
+            DisplayPatternA();
+            DisplayPatternB();
+            DisplayPatternC();
+            DisplayPatternD();
 
-		static void DisplayPatternA()
-		{
-			// your implementation here
+            Console.ReadLine();
+        }
+
+        static void DisplayPatternA()
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine(" Output A");
+            Console.WriteLine("");
+            int n = 10;
+            int i, j, k;
+
+            for (i = 1; i <= n; i++)
+            {
+
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+
+        static void DisplayPatternB()
+        {
+            int n = 10;
+            int i, j, k;
+
+            Console.WriteLine("");
+            Console.WriteLine(" Output B");
+            Console.WriteLine("");
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    // Console.Write(" ");
+                }
+                for (k = 1; k <= j; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void DisplayPatternC()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Output C");
+            Console.WriteLine("");
+            int n = 10;
+            int i, j, k;
+            for (i = 0; i < n; i++)
+            {
+                for (k = 0; k < i; k++)    //using i to control number of spaces
+                    Console.Write(" ");
+                for (j = n; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
 
 
-		}
 
-		static void DisplayPatternB()
-		{
-			// your implementation here
-		}
 
-		static void DisplayPatternC()
-		{
-			// your implementation here
-		}
+        }
 
-		static void DisplayPatternD()
-		{
-			// your implementation here
-		}
-	}
+        static void DisplayPatternD()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Output D");
+            Console.WriteLine("");
+
+            int n = 10;
+            int i, j, k;
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
 }
